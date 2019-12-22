369ae871d210932b1488864025944ba3e34f53f8
memotiur <memotiur@gmail.com>
2019-12-22 20:51:28 +0600

    table and inbox added

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     console.log("ready!");
 
-
+/*
     $(".main-content").slimScroll({
         size: '1px',
         width: '100%',
-        height: '100vh',
+        height: '110vh',
         color: '#3285EF',
         bottom:"0",
         allowPageScroll: true,
@@ -14,12 +14,12 @@ $(document).ready(function () {
     $(".side-nav").slimScroll({
         size: '1px',
         width: '250px',
-        height: '100vh',
+        height: '110vh',
         color: '#3285EF',
         bottom:"0",
         allowPageScroll: true,
         alwaysVisible: false
-    });
+    });*/
 
     $("#metismenu").metisMenu({
         toggle: false,
@@ -31,6 +31,7 @@ $(document).ready(function () {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+
 
     feather.replace();
 });
